65c613cdeaf6cf8e11b7e9d081028397da94b9b5
Kapil Verma <kapv89@gmail.com>
2022-11-06 20:14:55 +0530

    reference code for persistence to redis

--- src/redis.ts
+++ src/redis.ts
@@ -1,4 +1,5 @@
 import Redis from 'ioredis';
+import * as Y from 'yjs';
 import config from './config.js';
 import { WSSharedDoc } from './setupWSConnection.js';
 
@@ -6,7 +7,7 @@ const redis = new Redis(config.redis);
 
 export default redis;
 
-export const getDocUpdatesKey = (doc: WSSharedDoc) => `doc:${doc.id}:updates`;
+export const getDocUpdatesKey = (doc: WSSharedDoc) => `doc:${doc.id}:recent_updates`;
 
 export const getDocUpdatesFromQueue = async (doc: WSSharedDoc) => {
   return await redis.lrangeBuffer(getDocUpdatesKey(doc), 0, -1);
@@ -15,7 +16,7 @@ export const getDocUpdatesFromQueue = async (doc: WSSharedDoc) => {
 export const pushDocUpdatesToQueue = async (doc: WSSharedDoc, update: Uint8Array) => {
   const len = await redis.llen(getDocUpdatesKey(doc));
   if (len > 100) {
-    redis.pipeline()
+    await redis.pipeline()
       .lpopBuffer(getDocUpdatesKey(doc))
       .rpushBuffer(getDocUpdatesKey(doc), Buffer.from(update))
       .expire(getDocUpdatesKey(doc), 300)
@@ -25,5 +26,40 @@ export const pushDocUpdatesToQueue = async (doc: WSSharedDoc, update: Uint8Array
       .rpushBuffer(getDocUpdatesKey(doc), Buffer.from(update))
       .expire(getDocUpdatesKey(doc), 300)
       .exec();
+  }
+}
+
+export const getDocRedisKey = (docId: string) => `doc:${docId}:all_updates`
+
+export const docRedisLifetimeInS = 3600;
+
+export const saveDocUpdateInRedis = async (docId: string, update: Uint8Array) => {
+  const key = getDocRedisKey(docId);
+
+  const len = await redis.llen(key);
+  
+  if (len > 100) {
+    const res = await redis.pipeline()
+      .lrangeBuffer(key, 0, 100)
+      .rpushBuffer(key, Buffer.from(update))
+      .ltrim(key, 0, 100)
+      .exec()
+    ;
+
+    const [, leftUpdates]: [Error | null, Buffer[]] = res[0];
+
+    const doc = new Y.Doc();
+    for (const u of leftUpdates) {
+      Y.applyUpdate(doc, u);
+    }
+
+    const combinedUpdate = Y.encodeStateAsUpdate(doc);
+    await redis.lpushBuffer(key, Buffer.from(combinedUpdate));
+  } else {
+    await redis.pipeline()
+      .rpushBuffer(key, Buffer.from(update))
+      .expire(key, docRedisLifetimeInS)
+      .exec()
+    ;
   }
 }
